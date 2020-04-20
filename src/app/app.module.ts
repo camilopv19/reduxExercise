d9c3f57f03efa7e0cd935f187bf5cf9f70532926
Camilo Patiño Velez <cpatino@pactia.com>
2020-04-19 23:06:51 -0500

    Complex domains, refactored code with 2 new modules: Messaging and tasking, with the proper store and action files. Fixed paths and store selectors inside the components to point to right attributes of the new combined State (IAppState) in the main store. Usage of the redux method combineReducers and fixed shared module declaration in App.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, isDevMode } from '@angular/core';
 import { NgRedux, NgReduxModule, DevToolsExtension } from '@angular-redux/store';
 import { AppComponent } from './app.component';
-import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { IAppSate, rootReducer, INITIAL_STATE } from './store';
+
+import { TodosModule } from './tasking/todos.module';
+import { MessagingModule } from './messaging/messaging.module';
+import { IAppState, rootReducer, INITIAL_STATE } from './store';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoDashboardComponent,
-    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    NgReduxModule
+    NgReduxModule,
+    TodosModule,
+    MessagingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(ngRedux: NgRedux<IAppSate>,
+  constructor(ngRedux: NgRedux<IAppState>,
     devTools: DevToolsExtension) {
       var enhancers = isDevMode() ? [devTools.enhancer()] : [];
     ngRedux.configureStore(rootReducer, INITIAL_STATE, [], enhancers);  //With DevtoolsExtension, no need to add middleware
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,60 +1,18 @@
-import { ADD_TODO, REMOVE_TODO, CLEAR_TODO, COMPLETE_TODO } from './actions';
-import { tassign } from 'tassign';
+import { combineReducers } from "redux";
+import { ITaskingState, TASKING_INITIAL_STATE, taskingReducer } from './tasking/store';
+import { IMessagingState, MSG_INITIAL_STATE, messagingReducer } from './messaging/store';
 
-export interface IAppSate {
-    todos: any[],
-    lastUpdate: Date
+export interface IAppState {
+  tasking: ITaskingState,
+  messaging: IMessagingState;
 }
 
-export const INITIAL_STATE: IAppSate = {
-    lastUpdate: new Date(),
-    todos: []
-};
-
-function addTodo(state, action) {
-    let newTodo = { id: state.todos.length + 1, title: action.title };
-    let newState = {
-        todos: state.todos.concat(newTodo),
-        lastUpdate: new Date()
-    };
-    return tassign(state, newState);
-}
-function completeTodo(state, action) {
-    let st = state.todos;
-    let item = st.find(t => { return t.id == action.id }); //Gets the item with specific Id
-    let index = st.indexOf(item);
-
-    let before = st.slice(0, index); //Returns the portion from 0 to index
-    let after = st.slice(index + 1); //Returns the portion starting after index
-
-    let updated = tassign(item, { isCompleted: !item.isCompleted }); //Add the property with the toggled state 'isCompleted'
-
-    return tassign(state, {
-        todos: [...before, updated, ...after],
-        lastUpdate: new Date()
-    });
+export const INITIAL_STATE: IAppState = { 
+  tasking: TASKING_INITIAL_STATE,
+  messaging: MSG_INITIAL_STATE
 }
 
-function removeTodo(state, action) {
-    return tassign(state, {
-        todos: state.todos.filter(t => { return t.id != action.id }),
-        lastUpdate: new Date()
-    });
-}
-
-function clearTodo(state) {
-    return tassign(state, {
-        todos: [],
-        lastUpdate: new Date()
-    });
-}
-
-export function rootReducer(state: IAppSate, action) {
-    switch (action.type) {
-        case ADD_TODO: return addTodo(state, action);
-        case COMPLETE_TODO: return completeTodo(state, action);
-        case REMOVE_TODO: return removeTodo(state, action);
-        case CLEAR_TODO: return clearTodo(state);
-    }
-    return state;
-}
+export const rootReducer= combineReducers({
+  tasking: taskingReducer,
+  messaging: messagingReducer
+});
